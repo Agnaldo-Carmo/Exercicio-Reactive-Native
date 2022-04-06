522491db1b91973e67d5a868b69cce806c9805ce
Agnaldo do Carmo Carcereri Souza <79548453+Agnaldo-Carmo@users.noreply.github.com>
2022-04-06 12:41:39 -0300

    Update Portugues.js

--- src/components/Portugues.js
+++ src/components/Portugues.js
@@ -6,7 +6,7 @@ import Input from './Input';
 
 export const Portugues = (props) => {
   return (
-    <View style={[styles.container, {backgroundColor: 'red'}]}>
+    <View style={[styles.container, {backgroundColor: props.backColor}]}>
 
     <View>
       <Image style={{height:242}, {width:230}} source={require('../../CRF.png')} />
